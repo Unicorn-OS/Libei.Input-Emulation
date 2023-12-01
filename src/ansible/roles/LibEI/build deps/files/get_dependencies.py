e839210bff64bba327fac6bbff443b6c93e38611
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-12-01 03:34:55 -0600

    deps

---
+++ src/ansible/roles/LibEI/build deps/files/get_dependencies.py
@@ -0,0 +1,9 @@
+# Uses gitlab.ci file to generate dependencies:
+# https://gitlab.freedesktop.org/libinput/libei/-/blob/main/.gitlab-ci.yml
+
+pkg = "git diffutils gcc gcc-c++ pkgconf-pkg-config systemd-devel libxkbcommon-devel libxml2 doxygen python3-attrs python3-pytest python3-dbusmock python3-jinja2 python3-pip python3-pyyaml hugo libabigail"
+
+arr = pkg.split(' ')
+
+for i in arr:
+    print(f"- {i}")
